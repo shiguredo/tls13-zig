03377dfa066eafbd78713f9af649876cfc0852f2
Naoki MATSUMOTO <naoki@pibvt.net>
2022-08-25 19:21:21 +0900

    add SupportedGroups encoder

--- src/groups.zig
+++ src/groups.zig
@@ -37,6 +37,19 @@ pub const SupportedGroups = struct {
         return res;
     }
 
+    pub fn encode(self: Self, writer: anytype) !usize {
+        var len:usize = 0;
+        try writer.writeIntBig(u16, @intCast(u16, self.groups.items.len * @sizeOf(NamedGroup)));
+        len += @sizeOf(u16);
+
+        for (self.groups.items) |e| {
+            try writer.writeIntBig(u16, @enumToInt(e));
+            len += @sizeOf(NamedGroup);
+        }
+
+        return len;
+    }
+
     pub fn length(self: Self) usize {
         var len: usize = 0;
         len += @sizeOf(u16); // supported groups length
@@ -71,4 +84,21 @@ test "SupportedGroups decode" {
     try expect(res.groups.items[2] == .x448);
     try expect(res.groups.items[3] == .secp521r1);
     try expect(res.groups.items[4] == .secp384r1);
+}
+
+test "SupportedGroups encode" {
+    var res = SupportedGroups.init(std.testing.allocator);
+    defer res.deinit();
+
+    try res.groups.append(.x25519);
+    try res.groups.append(.secp256r1);
+    try res.groups.append(.x448);
+    try res.groups.append(.secp521r1);
+    try res.groups.append(.secp384r1);
+
+    const groups_ans = [_]u8{ 0x00, 0x0a, 0x00, 0x1d, 0x00, 0x17, 0x00, 0x1e, 0x00, 0x19, 0x00, 0x18 };
+    var send_bytes: [100]u8 = undefined;
+
+    const write_len = try res.encode(io.fixedBufferStream(&send_bytes).writer());
+    try expect(std.mem.eql(u8, send_bytes[0..write_len], &groups_ans));
 }

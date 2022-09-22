6a34bb4c4ac0c91c9b903d9f1f2ea6761e7f1d14
Naoki MATSUMOTO <naoki@pibvt.net>
2022-09-23 01:07:50 +0900

    test/gen_cert.sh: convert certificate and key to der formatted files

--- test/gen_cert.sh
+++ test/gen_cert.sh
@@ -3,5 +3,7 @@
 #openssl req -x509 -nodes -days 365 -subj '/C=JP/ST=Osaka/L=Kyoto/CN=example.com' -newkey rsa:2048 -keyout prikey.pem -out cert.pem
 openssl req -x509 -nodes -days 365 -subj '/C=JP/ST=Kyoto/L=Kyoto/CN=localhost' -newkey ec:<(openssl ecparam -name prime256v1) -nodes -sha256 -keyout prikey.pem -out cert.pem
 openssl x509 -text -noout -in cert.pem
+openssl x509 -outform der -in cert.pem -out cert.der
+openssl ec -outform der -in prikey.pem -out prikey.der
 #openssl x509 -outform der -in cert.pem -out cert.der
 #hexdump cert.der -v -e '13/1 "0x%02x, "' -e '"\n"'

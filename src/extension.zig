4800b7163f4c23f6ddd2027f8a12f279684f49cb
Naoki MATSUMOTO <naoki@pibvt.net>
2022-09-23 00:58:04 +0900

    extension.zig add Extensions as dummy

--- src/extension.zig
+++ src/extension.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const utils = @import("utils.zig");
 const NamedGroupList = @import("supported_groups.zig").NamedGroupList;
 const SupportedVersions = @import("supported_versions.zig").SupportedVersions;
 const SignatureSchemeList = @import("signature_scheme.zig").SignatureSchemeList;
@@ -38,13 +39,13 @@ const HandshakeType = @import("handshake.zig").HandshakeType;
 pub const ExtensionType = enum(u16) {
     server_name = 0,
     // max_fragment_length = 1,
-    // status_request = 5,
+    status_request = 5,
     supported_groups = 10,
     signature_algorithms = 13,
     // user_srtp = 14,
     // heartbeat = 15,
-    // application_layer_protocol_negotiation = 16,
-    // signed_certificate_timestamp = 18,
+    application_layer_protocol_negotiation = 16,
+    signed_certificate_timestamp = 18,
     // client_certificate_type = 19,
     // server_certificate_type = 20,
     // padding = 21,
@@ -53,13 +54,22 @@ pub const ExtensionType = enum(u16) {
     // early_data = 42,
     supported_versions = 43,
     // cookie = 44,
-    // psk_key_exchange_modes = 45,
+    psk_key_exchange_modes = 45,
     // certificate_authorities = 47,
     // oid_filters = 48,
-    // post_handshake_auth = 49,
+    post_handshake_auth = 49,
     // signature_algorithms_cert = 50,
     key_share = 51,
     none = 65535,
+
+    ec_points_format = 11,
+    next_protocol_negotiation = 13172,
+    encrypt_then_mac = 22,
+    extended_master_secret = 23,
+    padding = 21,
+    session_ticket = 35,
+    compress_certificate = 27,
+    application_settings = 17513,
 };
 
 /// RFC8446 Section 4.2 Extensions
@@ -76,7 +86,20 @@ pub const Extension = union(ExtensionType) {
     record_size_limit: RecordSizeLimit,
     supported_versions: SupportedVersions,
     key_share: KeyShare,
-    none: [0]u8,
+    none: Dummy,
+    application_layer_protocol_negotiation: Dummy,
+    psk_key_exchange_modes: Dummy,
+    post_handshake_auth: Dummy,
+    ec_points_format: Dummy,
+    next_protocol_negotiation: Dummy,
+    encrypt_then_mac: Dummy,
+    extended_master_secret: Dummy,
+    padding: Dummy,
+    status_request: Dummy,
+    signed_certificate_timestamp: Dummy,
+    session_ticket: Dummy,
+    compress_certificate: Dummy,
+    application_settings: Dummy,
 
     const Self = @This();
     pub const HEADER_LENGTH = @sizeOf(u16) + @sizeOf(u16);
@@ -89,14 +112,19 @@ pub const Extension = union(ExtensionType) {
     /// @return decoded Extension.
     pub fn decode(reader: anytype, allocator: std.mem.Allocator, ht: HandshakeType, hello_retry: bool) !Self {
         // Decoding ExtensionType.
-        const t = @intToEnum(ExtensionType, try reader.readIntBig(u16));
+        // none is for GREASE.
+        const t_raw = try reader.readIntBig(u16);
+        var t = utils.intToEnum(ExtensionType, t_raw) catch blk: {
+            std.log.warn("Unknown ExtensionType 0x{x:0>4}", .{t_raw});
+            break :blk .none;
+        };
 
         // Decoding extension_data.
         const len = try reader.readIntBig(u16); // TODO: check readable length of reader
 
         // If the length is 0, 'none'.
         if (len == 0) {
-            return Self{ .none = [0]u8{} };
+            t = .none;
         }
 
         switch (t) {
@@ -106,7 +134,20 @@ pub const Extension = union(ExtensionType) {
             .record_size_limit => return Self{ .record_size_limit = try RecordSizeLimit.decode(reader) },
             .supported_versions => return Self{ .supported_versions = try SupportedVersions.decode(reader, ht) },
             .key_share => return Self{ .key_share = try KeyShare.decode(reader, allocator, ht, hello_retry) },
-            .none => unreachable,
+            .none => return Self{ .none = try Dummy.decode(reader, len) },
+            .application_layer_protocol_negotiation => return Self{ .application_layer_protocol_negotiation = try Dummy.decode(reader, len) },
+            .psk_key_exchange_modes => return Self{ .psk_key_exchange_modes = try Dummy.decode(reader, len) },
+            .post_handshake_auth => return Self{ .post_handshake_auth = try Dummy.decode(reader, len) },
+            .ec_points_format => return Self{ .ec_points_format = try Dummy.decode(reader, len) },
+            .next_protocol_negotiation => return Self{ .next_protocol_negotiation = try Dummy.decode(reader, len) },
+            .encrypt_then_mac => return Self{ .encrypt_then_mac = try Dummy.decode(reader, len) },
+            .extended_master_secret => return Self{ .extended_master_secret = try Dummy.decode(reader, len) },
+            .padding => return Self{ .padding = try Dummy.decode(reader, len) },
+            .status_request => return Self{ .status_request = try Dummy.decode(reader, len) },
+            .signed_certificate_timestamp => return Self{ .signed_certificate_timestamp = try Dummy.decode(reader, len) },
+            .session_ticket => return Self{ .session_ticket = try Dummy.decode(reader, len) },
+            .compress_certificate => return Self{ .compress_certificate = try Dummy.decode(reader, len) },
+            .application_settings => return Self{ .application_settings = try Dummy.decode(reader, len) },
         }
     }
 
@@ -137,6 +178,7 @@ pub const Extension = union(ExtensionType) {
             .supported_versions => |e| len += try e.encode(writer),
             .key_share => |e| len += try e.encode(writer),
             .none => unreachable,
+            else => unreachable,
         }
 
         return len;
@@ -156,7 +198,20 @@ pub const Extension = union(ExtensionType) {
             .record_size_limit => |e| return e.length() + len,
             .supported_versions => |e| return e.length() + len,
             .key_share => |e| return e.length() + len,
-            .none => return len,
+            .none => |e| return e.length() + len,
+            .application_layer_protocol_negotiation => |e| return e.length() + len,
+            .psk_key_exchange_modes => |e| return e.length() + len,
+            .post_handshake_auth => |e| return e.length() + len,
+            .ec_points_format => |e| return e.length() + len,
+            .next_protocol_negotiation => |e| return e.length() + len,
+            .encrypt_then_mac => |e| return e.length() + len,
+            .extended_master_secret => |e| return e.length() + len,
+            .padding => |e| return e.length() + len,
+            .status_request => |e| return e.length() + len,
+            .signed_certificate_timestamp => |e| return e.length() + len,
+            .session_ticket => |e| return e.length() + len,
+            .compress_certificate => |e| return e.length() + len,
+            .application_settings => |e| return e.length() + len,
         }
     }
 
@@ -181,6 +236,26 @@ pub const Extension = union(ExtensionType) {
             .record_size_limit => |e| e.print(),
             .supported_versions => |e| e.print(),
             .key_share => |e| e.print(),
+            else => unreachable,
         }
+    }
+};
+
+pub const Dummy = struct {
+    len: usize,
+
+    const Self = @This();
+
+    pub fn decode(reader: anytype, len: usize) !Self {
+        var i: usize = 0;
+        while (i < len) : (i += 1) {
+            _ = try reader.readByte();
+        }
+
+        return .{ .len = len };
+    }
+
+    pub fn length(self: Self) usize {
+        return self.len;
     }
 };

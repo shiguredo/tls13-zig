1b98b11a419efd2d4ef1d380626f5bb0b3ad0ab9
Naoki MATSUMOTO <naoki@pibvt.net>
2022-09-23 01:08:22 +0900

    test/run_client.sh: force curl to run with TLS1.3

--- test/run_client.sh
+++ test/run_client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #openssl s_client -tls1_3 localhost:443
-SSLKEYLOGFILE=/tmp/pre-master-secret.log curl https://localhost:8443 --insecure
+SSLKEYLOGFILE=/tmp/pre-master-secret.log curl https://localhost:8443 --insecure --tlsv1.3
